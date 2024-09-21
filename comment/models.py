1356d45aadef0090346040a2b5c61108d08ce189
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:47:37 +0500

    change

--- comment/models.py
+++ comment/models.py
@@ -8,3 +8,9 @@ class CommentModel(models.Model):
     name = models.TextField()
     user = models.ForeignField(User, on_delete=models.CASCADE)
     book = models.ForeignKey(BookModel, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
