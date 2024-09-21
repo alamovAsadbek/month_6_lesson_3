c1fbc0e256fee7c1b7162fe73436f95225d55c8d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:47:13 +0500

    change CommentModel class

--- comment/models.py
+++ comment/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from book.models import BookModel
 
 
 class CommentModel(models.Model):
-    pass
+    name = models.TextField()
+    user = models.ForeignField(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(BookModel, on_delete=models.CASCADE)
