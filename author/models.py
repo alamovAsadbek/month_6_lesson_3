4d460268c10ac9092779e1aa5d1c4015ea58e33b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:06:53 +0500

    change AuthorModel class

--- author/models.py
+++ author/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 
 class AuthorModel(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
+    name = models.CharField(max_length=500)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
