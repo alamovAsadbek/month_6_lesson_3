5b66d57874530460127f82128f1a49cd34a185a8
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:42:13 +0500

    changed BookModel

--- book/models.py
+++ book/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from author.models import AuthorModel
+
 
 class BookModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     status = models.BooleanField(default=False)
+    author = models.ForeignField(AuthorModel, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
