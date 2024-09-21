93dbff59147cd62dd742864f54da47b5a8ae61a0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:51:14 +0500

    change

--- author/admin.py
+++ author/admin.py
@@ -4,8 +4,10 @@ from .models import AuthorModel
 
 admin.site.register(AuthorModel)
 
-
-@admin.register(AuthorModel)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
-    link_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
+#
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
+#     link_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
+#
+#
+# admin.site.register(AuthorModel, AuthorAdmin)
