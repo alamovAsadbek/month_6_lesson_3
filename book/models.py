335e46096941fe62dc9ab56b62764423d210f5fc
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 12:10:45 +0500

    change BookModel class

--- book/models.py
+++ book/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class BookModel(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
