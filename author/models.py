5eee164f0b199fdf3968550d34fdeda12c6a8add
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:40:56 +0500

    changed AuthorModel to Author

--- author/models.py
+++ author/models.py
@@ -2,14 +2,22 @@ from django.db import models
 
 
 class AuthorModel(models.Model):
+    # Represents the author's full name
     name = models.CharField(max_length=500)
+    # Represents the author's first name
     first_name = models.CharField(max_length=50)
+    # Represents the author's last name
     last_name = models.CharField(max_length=50)
+    # Represents the author's username
     username = models.CharField(max_length=50)
+    # Indicates whether the author is active or not
     status = models.BooleanField(default=True)
 
+    # Timestamp of when the author was created
     created_at = models.DateTimeField(auto_now_add=True)
+    # Timestamp of when the author was last updated
     updated_at = models.DateTimeField(auto_now=True)
 
+    # Returns the author's name when the object is printed
     def __str__(self):
         return self.name
