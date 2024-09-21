b861d67203a9732183ec5ee112c99cc54907057a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:54:08 +0500

    change models

--- comment/models.py
+++ comment/models.py
@@ -6,8 +6,8 @@ from book.models import BookModel
 
 class CommentModel(models.Model):
     name = models.TextField()
-    user = models.ForeignField(User, on_delete=models.CASCADE)
-    book = models.ForeignField(BookModel, on_delete=models.CASCADE)
+    user = models.ForeignObject(User, on_delete=models.CASCADE)
+    book = models.ForeignObject(BookModel, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
