179f715eda412604d0b361f3fd93e0a466cb2566
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:53:45 +0500

    change

--- author/admin.py
+++ author/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 from .models import AuthorModel
 
-admin.site.register(AuthorModel)
 
-#
-# class AuthorAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
-#     link_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
-#
-#
-# admin.site.register(AuthorModel, AuthorAdmin)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
+    link_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'username')
+
+
+admin.site.register(AuthorModel, AuthorAdmin)
