d19d725d721e903b43dab3f8f94f73d760f3cb35
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 15:01:39 +0500

    change

--- book/admin.py
+++ book/admin.py
@@ -5,7 +5,7 @@ from book.models import BookModel
 
 @admin.register(BookModel)
 class BookModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'description', 'created_at', 'updated_at')
-    list_display_links = ('name', 'author', 'description', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'author', 'description', 'created_at', 'updated_at')
+    list_display_links = ('id', 'name', 'author', 'description', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
     search_fields = ('name', 'author', 'description')
--- comment/admin.py
+++ comment/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from comment.models import CommentModel
 
-admin.site.register(CommentModel)
+@admin.register(CommentModel)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'user', 'post', 'created_at', 'updated_at')
