b84238c859c65e8b59bb08432292cb570e8b5e2a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:54:29 +0500

    change book models

--- book/models.py
+++ book/models.py
@@ -7,7 +7,7 @@ class BookModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     status = models.BooleanField(default=False)
-    author = models.ForeignField(AuthorModel, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignObject(AuthorModel, on_delete=models.SET_NULL, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
