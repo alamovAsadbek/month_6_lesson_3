ba3e930f5fdcbba524ebba36b6f3e5c6abd5b148
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:17:10 +0500

    change admin.py

--- author/admin.py
+++ author/admin.py
@@ -7,6 +7,7 @@ admin.site.register(AuthorModel)
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'created_at', 'updated_at')
+    link_display = ('name', 'email', 'phone', 'created_at', 'updated_at')
     search_fields = ('name', 'email', 'phone')
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
