4950dc54d02a4fe633480a6d29f279b6ddf0f322
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 15:00:09 +0500

    change

--- book/admin.py
+++ book/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from book.models import BookModel
 
-admin.site.register(BookModel)
+
+@admin.register(BookModel)
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'description', 'created_at', 'updated_at')
+    link_display_links = ('name', 'author', 'description', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'author', 'description')
