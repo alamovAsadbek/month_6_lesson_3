8c267518678b9c565e762015430156e09e4584fd
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 15:00:57 +0500

    change admin.py

--- book/admin.py
+++ book/admin.py
@@ -6,6 +6,6 @@ from book.models import BookModel
 @admin.register(BookModel)
 class BookModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'description', 'created_at', 'updated_at')
-    link_display_links = ('name', 'author', 'description', 'created_at', 'updated_at')
+    list_display_links = ('name', 'author', 'description', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
     search_fields = ('name', 'author', 'description')
