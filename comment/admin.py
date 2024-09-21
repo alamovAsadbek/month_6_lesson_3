672d31db2d51a202969de445bc5599aedfb6a297
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 15:15:14 +0500

    change

--- comment/admin.py
+++ comment/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from comment.models import CommentModel
+
+admin.site.site_header = 'Book Review Admin'
+
+admin.site.unregister(Group)
 
 
 @admin.register(CommentModel)
