cbaf4a026169b8b2f2da4813f8dc912364e61c93
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:46:36 +0500

    change

--- author/admin.py
+++ author/admin.py
@@ -7,6 +7,5 @@ admin.site.register(AuthorModel)
 
 @admin.register(AuthorModel)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
-    link_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
-    search_fields = ('first_name', 'last_name', 'username')
+    list_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
+    link_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
