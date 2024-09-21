40349fd30e838221a91b8a76f7c702f566342477
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:26:26 +0500

    change

--- author/admin.py
+++ author/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from author.models import AuthorModel
+from .models import AuthorModel
 
 admin.site.register(AuthorModel)
 
@@ -10,5 +10,3 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
     link_display = ('first_name', 'last_name', 'username', 'created_at', 'updated_at')
     search_fields = ('first_name', 'last_name', 'username', 'phone')
-    list_filter = ('created_at', 'updated_at')
-    readonly_fields = ('created_at', 'updated_at')
