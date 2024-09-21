541e829df0b4f4669d13cf65c73eb7614aa708b2
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 14:55:09 +0500

    change

--- author/admin.py
+++ author/admin.py
@@ -4,8 +4,8 @@ from .models import AuthorModel
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
-    link_display = ('first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
+    list_display = ('id', 'first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
+    link_display = ('id', 'first_name', 'last_name', 'username', 'status', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
     search_fields = ('first_name', 'last_name', 'username')
 
