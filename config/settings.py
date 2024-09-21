4ee7fe30055b816c6fcc3db06a3b2e61368d3cf5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-21 13:52:35 +0500

    change setting.py

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'author',
+    'book',
+    'comment'
 ]
 
 MIDDLEWARE = [
